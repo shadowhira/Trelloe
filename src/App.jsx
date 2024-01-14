2da4fffa2dcfd517c5636a7d57e05814337c7a04
shadowhira <thanhoc890@gmail.com>
2024-01-14 22:20:50 +0700

    add theme #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import Button from '@mui/material/Button'
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from '@mui/icons-material/Delete'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Hahahaha hom nay toi o nha</div>
+
+      <Typography variant="body2" color="text.secondary">Test Typo</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
       <br></br>
-      <DeleteIcon />      
+      <DeleteIcon />
     </>
   )
 }
