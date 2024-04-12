eae4e7279420981eabc9b415f460321bb8b12da0
shadowhira <thanhoc890@gmail.com>
2024-04-12 19:26:43 +0700

    add route

---
+++ src/routes/v1/index.js
@@ -0,0 +1,16 @@
+// đại diện cho route v1
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+
+const Router = express.Router()
+
+// Check api/v1/status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
+})
+
+// Board APIs
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
