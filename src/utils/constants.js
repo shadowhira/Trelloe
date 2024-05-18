682d426011400f9392011830d0edaf4e09068c1c
shadowhira <thanhoc890@gmail.com>
2024-05-18 10:50:12 +0700

    fix BUILD_MODE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,13 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+if (process. env. BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost: 8017'
+}
+if (process. env. BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-0gbu.onrender.com'
+}
+
+export const API_ROOT = apiRoot
+// export const API_ROOT = 'http://localhost:8017'
 // export let API_ROOT = 'https://trello-api-z8ri.onrender.com'
 
 // constants.js
