73ebf8cd70eddb2498925cbf7713911f605ccc44
shadowhira <thanhoc890@gmail.com>
2024-05-21 18:55:15 +0700

    complete v2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,14 @@
-// let apiRoot = ''
-// if (process. env. BUILD_MODE === 'dev') {
-//   apiRoot = 'http://localhost: 8017'
-// }
-// if (process. env. BUILD_MODE === 'production') {
-//   apiRoot = 'https://trello-api-0gbu.onrender.com'
-// }
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-0gbu.onrender.com'
+}
 
-// export const API_ROOT = apiRoot
+export const API_ROOT = apiRoot
 
-export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'http://localhost:8017'
 
 // export let API_ROOT = 'https://trello-api-z8ri.onrender.com'
 
