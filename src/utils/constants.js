04a9f0dd577736c5671a5550137d550e92a14a61
shadowhira <thanhoc890@gmail.com>
2024-05-21 19:04:21 +0700

    change apiRoot

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-0gbu.onrender.com'
+  apiRoot = 'https://trello-api-z8ri.onrender.com'
 }
 
 export const API_ROOT = apiRoot
