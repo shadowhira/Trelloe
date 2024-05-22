c1bfd01f2d3715b71bbf42b863d01786eb5cb709
shadowhira <thanhoc890@gmail.com>
2024-05-22 08:05:05 +0700

    move scroll BoardContent

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -338,7 +338,7 @@ function Board() {
 
   return (
     <Container disableGutters maxWidth={false}
-      sx={{ height: (theme) => theme.trello.boardContentHeight, backgroundColor: 'primary.main' }}
+      sx={{ height: '100vh', backgroundColor: 'primary.main' }}
     >
       <AppBar />
       <Box sx={{ display: 'flex' }}>
